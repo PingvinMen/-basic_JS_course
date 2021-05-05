1e3b0f8dc9241db05e1071d1d5af12da79dfa3d1
PingvinMen <55557719+PingvinMen@users.noreply.github.com>
2021-05-05 17:22:28 +0300

    Урок урок 1 задание 1, 2, 3, 4, 5

---
+++ lesson_1/3.js
@@ -0,0 +1,6 @@
+var admin, name;
+
+name = "Василий";
+admin = name;
+
+alert("Задания 3: \n" + "admin = " + admin);
---
+++ lesson_1/4.js
@@ -0,0 +1,7 @@
+var a, b, result;
+
+a = 1000;
+b = "108";
+result = a + b;
+
+alert("Задания 4: \n" + "1000 + \"108\" = " + result);
