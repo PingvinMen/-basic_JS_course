112fc85c4c3c00507880272399896c1c516d24a4
PingvinMen <55557719+PingvinMen@users.noreply.github.com>
2021-05-27 16:57:59 +0300

    Задание 6

---
+++ lesson_6/11.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+    var images = document.getElementsByTagName("img");
+    for(var i=0;i<images.length;i++){
+        images[i].onclick=showBigPng;
+    }
+}
+function showBigPng(event){
+    var bigPngBlock = document.getElementById("bigPng");
+    bigPngBlock.innerHTML="";
+    var minPng = event.target;
+    var imageNameParts = minPng.id.split("_");
+    var path = 'png/' + imageNameParts[0] + 'big.jpg';
+    var bigPng = document.createElement('img');
+    bigPng.style.display = "flex";
+    bigPng.src = path;
+    bigPngBlock.appendChild(bigPng);
+    bigPng.onerror = function(){
+        bigPng.style.display = "none";
+        alert("file not found");
+    }
+}
